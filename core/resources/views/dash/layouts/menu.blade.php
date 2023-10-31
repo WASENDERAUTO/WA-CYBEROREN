f19b2e25930aebf352f858c81e634998b5df4635
jxxzy <greensento@gmail.com>
2023-10-31 17:54:03 +0700

    update

--- core/resources/views/dash/layouts/menu.blade.php
+++ core/resources/views/dash/layouts/menu.blade.php
@@ -4,7 +4,7 @@
             <span class="app-brand-logo demo" style="height: unset">
                 <img style="height: 30px" src="{!! asset('assets/img/logo.png') !!}" alt="walix">
             </span>
-            <span class="app-brand-text demo menu-text fw-bold">WALazy</span>
+            <span class="app-brand-text demo menu-text fw-bold">WA-CYBEROREN</span>
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
